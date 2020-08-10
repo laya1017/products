a803f71ec8672479e9971e234f4f3e6fcc6f2bd8
YiChen <spi791017@gmail.com>
2020-08-10 22:03:41 +0800

    add catalog

--- products.py
+++ products.py
@@ -7,4 +7,8 @@ while True:
 	products.append([name, price])
 print(products)
 for p in products:
-	print(p[0], '的價格是',p[1])
+	print(p[0], '的價格是', p[1])
+with open('products.csv', 'w', encoding='utf-16') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
